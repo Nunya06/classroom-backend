3f50c322b1f45a5c3f51dee26aa2e2ee3921b743
Xorse Wisdom <wisdomxorse928@gmail.com>
2026-01-19 08:22:27 +0000

    implement GET all subjects routes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
 import express from "express";
+import cors from "cors";
+import subjectsRouter from "./routes/subjects";
+
 
 
 const app = express();
 const PORT = 8000;
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL, // React app URL
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+    credentials: true, // allow cookies
+  })
+);
+
 
 app.use(express.json());
+app.use('/api/subjects', subjectsRouter)
 
 app.get('/', (req, res) => {
     res.send('Backend server is running!');
